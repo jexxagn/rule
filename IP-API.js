b281c87d63bf30a45ebbaa8c82bfe6b71b5fa575
jexxagn <631217059@qq.com>
2022-06-23 06:17:16 +0000

    update IP-API.js.

--- IP-API.js
+++ IP-API.js
@@ -44,13 +44,10 @@ var flags = new Map([["AC","🇦🇨"],["AD","🇦🇩"],["AE","🇦🇪"],["AF"
 var body = $response.body;
 var obj = JSON.parse(body);
 var ip = obj['query'];
-function getCaption(obj){
-	var index=obj.lastIndexOf("/");
-	obj=obj.substring(index+1,obj.length);
-	return obj;
-}
 var zone= obj['timezone'];
+let index = zone .lastIndexOf("/");
+var zone =zone .substring(index+1,zone.length);
 var title = flags.get(obj['countryCode']) + ' ' + Area_check(obj['country']) + ' ' + City_ValidCheck(obj['regionName']) + ' ' + City_ValidCheck(obj['city']);
-var subtitle = '🏖 '+ getCaption(zone) + ' ➠ ' + obj['query'];
+var subtitle = '🏖 '+ zone + ' ➠ ' + obj['query'];
 var description = '-----------------------------------'+'\n'+'服务商:'+obj['isp'] + '\n\n'+'国家/地区:' +Area_check(obj['country']) + ' ' + City_ValidCheck(obj['regionName']) + ' ' + City_ValidCheck(obj['city'])+ '\n\n' + 'IP:'+ obj['query'] + '\n\n' +'经度:'+ obj['lon'] + '  ' + '维度:' + obj['lat'] +  '\n\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
