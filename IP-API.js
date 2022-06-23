ddab94b4d2167d0b479d2a3336407287c0bdae80
jexxagn <631217059@qq.com>
2022-06-23 14:16:19 +0000

    update IP-API.js.

--- IP-API.js
+++ IP-API.js
@@ -51,7 +51,7 @@ title = flags.get(obj['countryCode']) + ' ' + Area_check(obj['country']);
 }
 var oisp = obj['isp'];
 var subtitle;
-if(oisp.length>17){
+if(oisp.length>18){
 subtitle = '🏖 '+ obj['isp'];
 }
 else{
