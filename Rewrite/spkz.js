fafe38cc5b57ecec960abf66c86b4efed498420c
jexxagn <34477160+jexxagn@users.noreply.github.com>
2023-06-09 20:26:25 +0800

    Update spkz.js

--- Rewrite/spkz.js
+++ Rewrite/spkz.js
@@ -1,3 +1,2 @@
 let obj = JSON.parse($response.body);
-obj = {"data":{{"gonggao": {"open":"false"},"adconfignew": {"hengfu":"false","adtime":"999999999"}}};
-$done({body: JSON.stringify(obj)});
+obj = {"data":{{"gonggao": {"open":"false"},"adconfignew": {"hengfu":"false","adtime":"999999999"}}}
